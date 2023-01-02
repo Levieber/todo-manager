947b3daffa38657c527f754760492dca00739f5b
Levi Eber <gblevieber@gmail.com>
2023-01-02 14:17:05 -0300

    feat: tasks listing

---
+++ src/components/Task.tsx
@@ -0,0 +1,38 @@
+import styles from "./Task.module.css";
+import { TrashSimple as DeleteIcon, PencilSimple as EditIcon } from "phosphor-react";
+
+interface TaskProps {
+  id: number;
+  title: string;
+  done: boolean;
+  onToggleDoneTask: (id: number) => void;
+}
+
+export function Task({ id, title, done, onToggleDoneTask }: TaskProps) {
+  return (
+    <li className={styles.task}>
+      <div className={styles.taskTitleAndCheck}>
+        <input onChange={() => onToggleDoneTask(id)} type="checkbox" id={id.toString()} />
+
+        <label className={done ? styles.checked : undefined} htmlFor={id.toString()}>
+          {title}
+        </label>
+      </div>
+
+      <div className={styles.taskEditOrDelete}>
+        <button
+          className={styles.editTaskBtn}
+          aria-label={`Editar a tarefa ${title}`}
+          type="button"
+          disabled={done}
+        >
+          <EditIcon size={25} />
+        </button>
+
+        <button className={styles.deleteTaskBtn} aria-label={`Excluir a tarefa ${title}`} type="button">
+          <DeleteIcon size={25} />
+        </button>
+      </div>
+    </li>
+  );
+}
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import styles from "./TaskList.module.css";
 import { useState } from "react";
+import { Task } from "./Task";
 
 export function TaskList() {
   const [taskTitle, setTaskTitle] = useState("");
@@ -24,6 +25,12 @@ export function TaskList() {
   const handleInvalidTitle = (event: React.InvalidEvent<HTMLInputElement>) =>
     event.target.setCustomValidity("O título da tarefa não pode estar vazio!");
 
+  const handleToggleDoneTask = (id: number) => {
+    const newTasks = tasks.map((task) => (task.id === id ? { ...task, done: !task.done } : task));
+
+    setTasks(newTasks);
+  };
+
   const isTaskTitleEmpty = taskTitle.trim() === "";
 
   return (
@@ -42,6 +49,14 @@ export function TaskList() {
           Salvar
         </button>
       </form>
+      <section className={styles.taskContainer}>
+        <h2>Minhas tarefas:</h2>
+        <ul>
+          {tasks.map((task) => (
+            <Task key={task.id} onToggleDoneTask={handleToggleDoneTask} {...task} />
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
