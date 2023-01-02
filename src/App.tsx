be98f9afb4cf028d7a5697a159c98f76efdcc83e
Levi Eber <gblevieber@gmail.com>
2023-01-02 18:23:37 -0300

    feat: footer of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./global.css";
 import { Header } from "./components/Header";
 import { TaskList } from "./components/TaskList";
+import { Footer } from "./components/Footer";
 
 export function App() {
   return (
     <div>
       <Header />
       <TaskList />
+      <Footer />
     </div>
   );
 }
