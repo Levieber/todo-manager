67da64488f0af2c27ec156d312af9618b78d08eb
Levi Eber <gblevieber@gmail.com>
2023-01-02 18:26:28 -0300

    feat: style for footer to be at the bottom.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Footer } from "./components/Footer";
 
 export function App() {
   return (
-    <div>
+    <div className="appContainer">
       <Header />
       <TaskList />
       <Footer />
