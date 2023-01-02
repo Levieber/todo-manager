aad7780c55f34fb1d16d13cd3d81a5209071a2da
Levi Eber <gblevieber@gmail.com>
2023-01-02 13:15:14 -0300

    feat: logic to add new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import styles from "./TaskList.module.css";
 
 export function TaskList() {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [tasks, setTasks] = useState<{ id: number; title: string; done: boolean }[]>([]);
+
+  const handleCreateTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newTask = {
+      id: new Date().getTime(),
+      title: taskTitle,
+      done: false,
+    };
+    setTasks([...tasks, newTask]);
+    setTaskTitle("");
+  };
+
   return (
     <main className={styles.container}>
-      <form className={styles.taskForm}>
-        <input id="task" type="text" placeholder="Digite uma tarefa. Ex: estudar React" />
+      <form onSubmit={handleCreateTask} className={styles.taskForm}>
+        <input
+          value={taskTitle}
+          onChange={(ev) => setTaskTitle(ev.target.value)}
+          id="task"
+          type="text"
+          placeholder="Digite uma tarefa. Ex: estudar React"
+        />
         <button type="submit">Salvar</button>
       </form>
     </main>
